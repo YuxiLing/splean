34198e3f22380ce6584057415a7676253dbd38ea
volodeyka <vovaglad00@gmail.com>
2024-06-19 14:51:58 +0800

    init

--- LeanLgtm/Basic.lean
+++ LeanLgtm/Basic.lean
@@ -1,1 +1,72 @@
-def hello := "world"
+import Ssreflect.Lang
+
+
+/-
+Inductive prim : Type :=
+  | val_ref : prim
+  | val_get : prim
+  | val_set : prim
+  | val_free : prim
+  | val_neg : prim
+  | val_opp : prim
+  | val_eq : prim
+  | val_add : prim
+  | val_neq : prim
+  | val_sub : prim
+  | val_mul : prim
+  | val_div : prim
+  | val_mod : prim
+  | val_rand : prim
+  | val_le : prim
+  | val_lt : prim
+  | val_ge : prim
+  | val_gt : prim
+  | val_ptr_add : prim.
+-/
+inductive prim where
+  | val_ref : prim
+  | val_get : prim
+  | val_set : prim
+  | val_free : prim
+  | val_neg : prim
+  | val_opp : prim
+  | val_eq : prim
+  | val_add : prim
+  | val_neq : prim
+  | val_sub : prim
+  | val_mul : prim
+  | val_div : prim
+  | val_mod : prim
+  | val_rand : prim
+  | val_le : prim
+  | val_lt : prim
+  | val_ge : prim
+  | val_gt : prim
+  | val_ptr_add : prim
+
+abbrev loc := Nat
+abbrev var := String
+
+def null : loc := 0
+
+
+inductive val where
+  | val_unit : val
+  | val_bool : Bool → val
+  | val_int : Int → val
+  | val_loc : loc → val
+  | val_prim : prim → val
+  | val_fun : var -> trm -> val
+  | val_fiv : var -> var -> trm -> val
+  | val_uninit : val
+  | val_error : val
+  with
+inductive trm where
+  | trm_val : val -> trm
+  | trm_var : var -> trm
+  | trm_fun : var -> trm -> trm
+  | trm_fix : var -> var -> trm -> trm
+  | trm_app : trm -> trm -> trm
+  | trm_seq : trm -> trm -> trm
+  | trm_let : var -> trm -> trm -> trm
+  | trm_if : trm -> trm -> trm -> trm
--- lakefile.lean
+++ lakefile.lean
@@ -1,16 +1,19 @@
 import Lake
 open Lake DSL
 
-package «lean-lgtm» where
+package lean_lgtm where
   -- Settings applied to both builds and interactive editing
   leanOptions := #[
     ⟨`pp.unicode.fun, true⟩ -- pretty-prints `fun a ↦ b`
   ]
   -- add any additional package configuration options here
 
-require mathlib from git
-  "https://github.com/leanprover-community/mathlib4.git"
+require batteries from
+    git "https://github.com/leanprover-community/batteries" @ "main"
+require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "master"
+require loogle from git "https://github.com/nomeata/loogle.git" @ "master"
+require ssreflect from git "https://github.com/verse-lab/lean-ssr.git" @ "master"
 
 @[default_target]
-lean_lib «LeanLgtm» where
+lean_lib LeanLgtm where
   -- add any library configuration options here
