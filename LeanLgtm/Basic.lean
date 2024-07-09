e3c8fe2aaeeffa2c86877390ca15665c1f21d380
swang-1 <swang6987@gmail.com>
2024-07-09 15:53:32 +0800

    wpgen

--- LeanLgtm/Basic.lean
+++ LeanLgtm/Basic.lean
@@ -2037,3 +2037,254 @@ by
   move=> ?
   srw Not -[]mem_keys !mem_erase => [] ? [] ??
   sby srw mem_union []mem_insert
+
+
+/- ------------------ Definition of [wpgen] ------------------ -/
+
+/- Defining [mkstruct] -/
+
+abbrev formula := (val → hprop) → hprop
+
+/- [mkstruct F] transforms a formula [F] into one satisfying structural
+   rules of Separation Logic. -/
+
+def mkstruct (F : formula) :=
+  fun Q ↦ h∃ Q', F Q' ∗ (Q' -∗∗ Q)
+
+lemma mkstruct_ramified Q1 Q2 F :
+  (mkstruct F Q1) ∗ (Q1 -∗∗ Q2) ==> (mkstruct F Q2) :=
+by
+  srw []mkstruct
+  sorry --xsimp
+
+lemma mkstruct_erase Q F :
+  F Q ==> mkstruct F Q :=
+by
+  srw mkstruct
+  sorry --xsimp
+
+lemma mkstruct_conseq F Q1 Q2 :
+  Q1 ===> Q2 →
+  mkstruct F Q1 ==> mkstruct F Q2 :=
+by
+  srw []mkstruct => ?
+  sorry --xpull
+
+lemma mkstruct_frame F H Q :
+  (mkstruct F Q) ∗ H ==> mkstruct F (Q ∗∗ H) :=
+by
+  srw []mkstruct
+  sorry --xpull
+
+lemma mkstruct_monotone F1 F2 Q :
+  (forall Q, F1 Q ==> F2 Q) →
+  mkstruct F1 Q ==> mkstruct F2 Q :=
+by
+  move=> ?
+  srw []mkstruct
+  sorry --xpull
+
+
+/- Auxiliary Definitions for [wpgen] -/
+
+def wpgen_fail : formula :=
+  fun _ ↦ ⌜false⌝
+
+def wpgen_val (v : val) : formula :=
+  fun Q ↦ Q v
+
+def wpgen_fun (Fof : val → formula) : formula :=
+  fun Q ↦ h∀ vf, ⌜forall vx Q', Fof vx Q' ==>
+    wp (trm_app (trm_val vf) (trm_val vx)) Q'⌝ -∗ Q vf
+
+def wpgen_fix (Fof : val → val → formula) : formula :=
+  fun Q ↦ h∀ vf, ⌜forall vx Q', Fof vf vx Q' ==>
+    wp (trm_app vf vx) Q'⌝ -∗ Q vf
+
+def wpgen_var (E : ctx) (x : var) : formula :=
+  match lookup x E with
+  | none   => wpgen_fail
+  | some v => wpgen_val v
+
+def wpgen_seq (F1 F2 : formula) : formula :=
+  fun Q ↦ F1 (fun _ ↦ F2 Q)
+
+def wpgen_let (F1 : formula) (F2of : val → formula) : formula :=
+  fun Q ↦ F1 (fun v ↦ F2of v Q)
+
+def wpgen_if (t : trm) (F1 F2 : formula) : formula :=
+  fun Q ↦ h∃ (b : Bool),
+    ⌜t = trm_val (val_bool b)⌝ ∗ (if b then F1 Q else F2 Q)
+
+def wpgen_if_trm (F0 F1 F2 : formula) : formula :=
+  wpgen_let F0 (fun v ↦ mkstruct (wpgen_if v F1 F2))
+
+def wpgen_app (t : trm) : formula :=
+  fun Q ↦ h∃ H, H ∗ ⌜triple t H Q⌝
+
+
+/- Recursive Definition of [wpgen] -/
+
+def wpgen (E : ctx) (t : trm) : formula :=
+  mkstruct (match t with
+  | trm_val v       => wpgen_val v
+  | trm_var x       => wpgen_var E x
+  | trm_fun x t1    => wpgen_fun (fun v ↦ wpgen (insert x v E) t1)
+  | trm_fix f x t1  => wpgen_fix
+      (fun vf v ↦ wpgen (insert f vf (insert x v E)) t1)
+  | trm_if t0 t1 t2 => wpgen_if (isubst E t0) (wpgen E t1) (wpgen E t2)
+  | trm_seq t1 t2   => wpgen_seq (wpgen E t1) (wpgen E t2)
+  | trm_let x t1 t2 => wpgen_let (wpgen E t1) (fun v ↦ wpgen (insert x v E) t2)
+  | trm_app _ _   => wpgen_app (isubst E t) )
+
+
+/- ---------------- Soundness of [wpgen] ---------------- -/
+
+def formula_sound (t : trm) (F : formula) : Prop :=
+  forall Q, F Q ==> wp t Q
+
+lemma wp_sound t :
+  formula_sound t (wp t) :=
+by
+  sby move=> ??
+
+/- Soundness lemma for [mkstruct] -/
+
+lemma mkstruct_wp t :
+  mkstruct (wp t) = wp t :=
+by
+  move=> ! ?
+  apply himpl_antisym
+  { srw mkstruct
+    sorry /- xpull -/ }
+  apply mkstruct_erase
+
+lemma mkstruct_sound t F :
+  formula_sound t F →
+  formula_sound t (mkstruct F) :=
+by
+  srw []formula_sound => ? ?
+  srw -mkstruct_wp
+  sby apply mkstruct_monotone=> ??
+
+/- Soundness lemmas for each term construct -/
+
+lemma wpgen_fail_sound t :
+  formula_sound t wpgen_fail :=
+by
+  move=> ?
+  srw wpgen_fail
+  sorry --xpull
+
+lemma wpgen_val_sound v :
+  formula_sound (trm_val v) (wpgen_val v) :=
+by
+  move=> ?
+  srw wpgen_val
+  apply wp_val
+
+lemma wpgen_fun_sound x t1 Fof :
+  (forall vx, formula_sound (subst x vx t1) (Fof vx)) →
+  formula_sound (trm_fun x t1) (wpgen_fun Fof) :=
+by
+  move=> ? ?
+  srw wpgen_fun
+  apply (himpl_hforall_l _ (val_fun x t1))
+  sorry -- xchange hwand_hpure_l
+
+lemma wpgen_fix_sound f x t1 Fof :
+  (forall vf vx, formula_sound (subst v vx (subst f vf t1)) (Fof vf vx)) →
+  formula_sound (trm_fix f x t1) (wpgen_fix Fof) :=
+by
+  move=> ? ?
+  srw wpgen_fix
+  apply (himpl_hforall_l _ (val_fix f x t1))
+  sorry -- xchange hwand_hpure_l
+
+lemma wpgen_seq_sound F1 F2 t1 t2 :
+  formula_sound t1 F1 →
+  formula_sound t2 F2 →
+  formula_sound (trm_seq t1 t2) (wpgen_seq F1 F2) :=
+by
+  srw []formula_sound => ?? Q
+  srw wpgen_seq
+  apply (himpl_trans (wp t1 (fun _ ↦ wp t2 Q)))
+  { apply (himpl_trans (wp t1 fun _ ↦ F2 Q))
+    move=> ? //
+    apply wp_conseq => ? /=
+    sdone }
+  apply wp_seq
+
+lemma wpgen_let_sound F1 F2of x t1 t2 :
+  formula_sound t1 F1 →
+  (forall v, formula_sound (subst x v t2) (F2of v)) →
+  formula_sound (trm_let x t1 t2) (wpgen_let F1 F2of) :=
+by
+  srw []formula_sound => ?? Q
+  srw wpgen_let
+  apply himpl_trans (wp t1 (fun v ↦ wp (subst x v t2) Q))
+  { apply himpl_trans (wp t1 (fun v ↦ F2of v Q ))
+    {  sby move=> ? }
+    apply wp_conseq => ? /=
+    sdone }
+  apply wp_let
+
+lemma wpgen_if_sound F1 F2 t0 t1 t2 :
+  formula_sound t1 F1 →
+  formula_sound t2 F2 →
+  formula_sound (trm_if t0 t1 t2) (wpgen_if t0 F1 F2) :=
+by
+  srw []formula_sound => ?? Q
+  srw wpgen_if
+  sorry -- xpull
+
+lemma wpgen_app_sound t :
+  formula_sound t (wpgen_app t) :=
+by
+ move=> ??
+ srw wpgen_app
+ sorry -- xpull
+
+/- Main soundness lemma -/
+
+lemma wpgen_sound E t :
+  formula_sound (isubst E t) (wpgen E t) :=
+by
+  -- elim: t
+  scase: t
+  any_goals move=> * ; srw isubst wpgen ; try apply mkstruct_sound
+  { apply wpgen_val_sound }
+  { srw wpgen_var
+    cases eqn:(lookup _ E)=> /=
+    { apply wpgen_fail_sound }
+    apply wpgen_val_sound }
+  { apply wpgen_fun_sound=> ?
+    srw -isubst_rem
+    sorry /- need induction -/ }
+  { apply wpgen_fix_sound=> *
+    srw -isubst_rem_2
+    sorry }
+  { srw isubst
+    apply wpgen_app_sound }
+  { apply wpgen_seq_sound
+    sorry ; sorry }
+  { apply wpgen_let_sound
+    sorry
+    move=> ?
+    srw -isubst_rem
+    sorry }
+  { apply wpgen_if_sound
+    sorry ; sorry }
+
+lemma himpl_wpgen_wp t Q :
+  wpgen ∅ t Q ==> wp t Q :=
+by
+  move=> ? /wpgen_sound
+  sby srw isubst_nil
+
+lemma triple_of_wpgen t H Q :
+  H ==> wpgen ∅ t Q →
+  triple t H Q :=
+by
+  srw -wp_equiv=> ?
+  sorry -- xchange
