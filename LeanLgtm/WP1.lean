10971bc00f70902382539bf6c5922571c81cae4f
volodeyka <vovaglad00@gmail.com>
2024-07-23 14:21:04 +0800

    make xapp hints consistent across files

--- LeanLgtm/WP1.lean
+++ LeanLgtm/WP1.lean
@@ -951,38 +951,3 @@ elab "xsimpr" : tactic => do
   xsimp_step_r (<- XSimpRIni)
 
 set_option linter.hashCommand false
-
-/- ################################################################# -/
-/-* * Demo Programs -/
-
-lang_def incr :=
-  fun p =>
-    let n := !p in
-    let m := n + 1 in
-    p := m
-
-@[xapp]
-lemma triple_incr (p : loc) (n : Int) :
-  {p ~~> n}
-  [incr(p)]
-  {p ~~> n + 1} := by
-  sdo 3 (xwp; xapp)
-
-lang_def mysucc :=
-  fun n =>
-    let r := ref n in
-    incr r;
-    let x := !r in
-    free r;
-    x
--- set_option pp.notation false
--- set_option pp.explicit true
--- set_option pp.funBinderTypes true
--- set_option trace.xsimp true
-
-lemma triple_mysucc (n : Int) :
-  { emp }
-  [mysucc n]
-  {v, ⌜ v = n + 1 ⌝} := by
-  sdo 4 (xwp; xapp);
-  xwp; xval; xsimp
