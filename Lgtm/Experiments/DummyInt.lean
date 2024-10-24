cc7da10ac7beb7d0e95dd00ced07fb294089d3fc
volodeyka <vovaglad00@gmail.com>
2024-10-24 14:48:30 +0800

    port current progress to region based memory

--- Lgtm/Experiments/DummyInt.lean
+++ Lgtm/Experiments/DummyInt.lean
@@ -30,7 +30,6 @@ lemma toReal_eq (i : ℝ) : val.toReal i = i := by rfl
 
 attribute [-simp] fun_insert Bool.forall_bool
 attribute [simp] Set.univ_inter
-#hint_yapp htriple_ref
 
 lang_def Lang.get :=
   fun xind xval i z n =>
@@ -39,8 +38,7 @@ lang_def Lang.get :=
 
 lang_def Lang.int :=
   fun xind xval z n =>
-    let ans := ref ⟨0 : ℝ⟩ in
-    -- let n' := n - 1 in
+    ref ans := ⟨0 : ℝ⟩ in
     for i in [z:n] {
       let v := xval[i] in
       let ind := xind[i] in
@@ -49,9 +47,7 @@ lang_def Lang.int :=
       let ln := ind' - ind in
       let v := v * ln in
       ans += v
-    };
-    let r := !ans in
-    free ans; r
+    }; !ans
 
 variable (xind xval : loc) (z n : ℤ) (N : ℕ)
 variable (x_ind : ℤ -> ℝ) (x_val : ℤ -> ℝ)
@@ -60,7 +56,6 @@ variable (x_ind : ℤ -> ℝ) (x_val : ℤ -> ℝ)
 #hint_yapp triple_hharrayFun_length
 #hint_yapp htriple_eq
 #hint_yapp htriple_get
-#hint_yapp htriple_free
 #hint_yapp htriple_subr
 #hint_yapp htriple_mulr
 #hint_yapp htriple_le
@@ -119,13 +114,13 @@ lemma intergal_spec (r : ℝ)  :
     ⌜v ⟨1,r⟩ = ∫ i in Set.Ico (x_ind z) (x_ind n), (v ⟨2,i⟩).toReal⌝ ∗
     arr⟨⋆⟩(xind, x in N => x_ind x) ∗
     arr⟨⋆⟩(xval, x in N => x_val x) } := by
-  yin 1: ystep=> ans;
+  yin 1: ywp; yref ans;
   srw x_ind_union //' (foo''' 2) ?(foo''' 0)
   let op := fun (hv : hval ℝˡ) i => ∫ j in Set.Ico (x_ind i) (x_ind (i + 1)), (hv ⟨2,j⟩).toReal
   let P := fun (hv : hval ℝˡ) i => IntegrableOn (fun j => (hv ⟨2,j⟩).toReal) (Set.Ico (x_ind i) (x_ind (i + 1)))
   yfor+. with
-    (Q := fun i hv => ans k ~⟨k in ⟪1, {r}⟫⟩~> op hv i ∗ ⌜P hv i⌝)
-    (H₀ := ans i ~⟨i in ⟪1, {r}⟫⟩~> val_real 0) <;> try simp [op, P]
+    (Q := fun i hv => ans ~⟨k in ⟪1, {r}⟫⟩~> op hv i ∗ ⌜P hv i⌝)
+    (H₀ := ans ~⟨i in ⟪1, {r}⟫⟩~> val_real 0) <;> try simp [op, P]
   { move=> j > /== ?? eq; congr! 2
     { congr! 2; apply MeasureTheory.setIntegral_congr
       { exact measurableSet_Ico }
@@ -141,7 +136,7 @@ lemma intergal_spec (r : ℝ)  :
       srw Real.volume_Ico mul_comm ENNReal.toReal_ofReal /==
       apply x_ind_montone=> /== //' }
     simp [Finset.mem_Ico ]=> //' }
-  ystep; ystep; ywp; yval; ysimp=> /==
+  yapp=> ?; ysimp=> /==
   srw MeasureTheory.integral_finset_biUnion //'
   simp=> i /== ?? j ?? ?
   scase: [i < j]=> ?
