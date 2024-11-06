87433c71543e051d7e0c633e7873f066b86c395a
Ilya Sergey <ilya@nus.edu.sg>
2024-11-06 13:07:13 +0800

    rw? is cheating

--- Lgtm/Common/LabType.lean
+++ Lgtm/Common/LabType.lean
@@ -26,7 +26,6 @@ lemma labSet_diff (l : ℕ) (s s' : Set α) :
   {aesop}
   aesop
 
-
 @[simp]
 lemma labSet_inter (l : ℕ) (s s' : Set α) :
   ⟪l, s⟫ ∩ ⟪l, s'⟫ = ⟪l, s ∩ s'⟫ := by
@@ -46,8 +45,7 @@ lemma disjoint_label_set :
   simp [labSet]; constructor
   {
     move=>H; by_cases L: (l = l')=>//
-    {
-      subst l'; right=>w H1 H2
+    { subst l'; right=>w H1 H2
       let z := {x | ∃ y ∈ w, Labeled.mk l y = x}
       shave G: z ≤ ⊥
       { apply H; simp [z]
@@ -55,7 +53,10 @@ lemma disjoint_label_set :
         aesop }
       move: G; simp[z]
       srw Set.eq_empty_iff_forall_not_mem /== => H
-      exact Set.subset_eq_empty H rfl
-    }
+      exact Set.subset_eq_empty H rfl }
   }
-  sorry
+  scase=>H; srw Set.disjoint_iff_inter_eq_empty Set.eq_empty_iff_forall_not_mem=>x//==
+  {aesop}
+  move=>y1 H1 [y2] H2 Z; cases Z
+  move: H
+  sby srw Set.disjoint_iff_inter_eq_empty Set.eq_empty_iff_forall_not_mem
