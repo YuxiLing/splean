7d672c2bebceda240963b3fb2cbe4f98757857a8
Ilya Sergey <ilya@nus.edu.sg>
2024-11-06 12:03:06 +0800

    some trivial admits removed

--- Lgtm/Common/LabType.lean
+++ Lgtm/Common/LabType.lean
@@ -16,22 +16,45 @@ def labSet (l : ℕ) (s : Set α) : Set (Labeled α) := { ⟨l, x⟩ | x ∈ s }
 
 notation (priority := high) "⟪" l ", " s "⟫" => labSet l s
 
-@[disjE]
-lemma disjoint_label_set :
-  Disjoint ⟪l,s⟫ ⟪l',s'⟫ ↔ l ≠ l' ∨ Disjoint s s' := by
-  sorry
+@[simp]
+lemma labSet_diff (l : ℕ) (s s' : Set α) :
+   ⟪l, s⟫ \ ⟪l, s'⟫ = ⟪l, s \ s'⟫ := by
+  srw !labSet; apply Set.ext=>x; constructor=>//=
+  -- { sby scase=>[w [H1 []]]Hn; exists w }
+  -- sby scase=>w[H []]
+  -- Who am I kidding? This how one should prove it:
+  {aesop}
+  aesop
+
 
 @[simp]
 lemma labSet_inter (l : ℕ) (s s' : Set α) :
   ⟪l, s⟫ ∩ ⟪l, s'⟫ = ⟪l, s ∩ s'⟫ := by
-  sorry
-
-@[simp]
-lemma labSet_diff (l : ℕ) (s s' : Set α) :
-   ⟪l, s⟫ \ ⟪l, s'⟫ = ⟪l, s \ s'⟫ := by
-  sorry
+  srw !labSet; apply Set.ext=>x; constructor=>//==
+  { aesop }
+  aesop
 
 @[simp]
 lemma labSet_mem :
   x ∈ ⟪l, s⟫ ↔ x.lab = l ∧ x.val ∈ s := by
+  simp [labSet]; constructor; aesop
+  sby scase=><-?
+
+@[disjE]
+lemma disjoint_label_set :
+  Disjoint ⟪l,s⟫ ⟪l',s'⟫ ↔ l ≠ l' ∨ Disjoint s s' := by
+  simp [labSet]; constructor
+  {
+    move=>H; by_cases L: (l = l')=>//
+    {
+      subst l'; right=>w H1 H2
+      let z := {x | ∃ y ∈ w, Labeled.mk l y = x}
+      shave G: z ≤ ⊥
+      { apply H; simp [z]
+        {aesop}
+        aesop }
+      move: G; simp[z]
+      sorry
+    }
+  }
   sorry
